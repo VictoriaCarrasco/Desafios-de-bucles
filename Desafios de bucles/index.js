e24941a06690de7b89ee630c056938f75d6f38ba
VictoriaCarrasco <133164750+VictoriaCarrasco@users.noreply.github.com>
2023-05-24 22:12:08 -0400

    Desafios de bucles js

---
+++ Desafios de bucles/index.js
@@ -0,0 +1,30 @@
+for (var i = 1; i < 20; i++) {
+    if ( i % 2 == 1 ) {
+    console.log(i);
+    }
+}
+
+
+for (var i = 1; i < 101; i++) {
+    if ( i % 3 == 0 ) {
+    console.log(i);
+    }
+}
+
+for (var i = 4; i > -4 ; i -= 1.5) {
+    console.log(i)
+}
+
+var sum=0
+for (var i = 1; i <= 100; i++) {
+    sum = sum + i;
+}
+    console.log(sum);
+    
+
+var product=1
+for (var i=1; i <= 12; i++) {
+    product = product * i;
+}
+
+console.log(product);
